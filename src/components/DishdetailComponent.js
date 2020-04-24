875170640c43fea1253e0c68290c28395f818951
Louis <emyhoang8899@gmail.com>
2020-04-24 12:35:38 -0700

    refactor class components in dishdetail and menu component to function comp
    - verify behavior is working as should be

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,43 +2,48 @@ import React from 'react';
 import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
 import DishComment from './DishComment';
 
-class DishDetail extends React.Component {
-  renderComment = () =>
-    this.props.dishSelected.comments.map((comment) => {
-      return (
-        <ul key={comment.id} className='list-unstyled'>
-          <DishComment {...comment} />
-        </ul>
-      );
-    });
+const RenderComment = (props) => {
+  console.log(`RenderComment ${JSON.stringify(props)}`);
 
-  render() {
-    const { dishSelected: dish } = this.props;
+  return props.dishSelected.comments.map((comment) => {
+    return (
+      <ul key={comment.id} className='list-unstyled'>
+        <DishComment {...comment} />
+      </ul>
+    );
+  });
+};
 
-    if (dish != null) {
-      return (
-        <div className='container'>
-          <div className='row'>
-            <div className='col-12 col-md-5 mt-1'>
-              <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                  <CardTitle className='font-weight-bold'>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
-              </Card>
-            </div>
-            <div className='col col-md mt-1'>
-              <h4>Comments</h4>
-              <div className='font-weight-bold'>{this.renderComment()}</div>
+const DishDetail = (props) => {
+  console.log(`DishDetail ${JSON.stringify(props)}`);
+
+  const { dishSelected: dish } = props;
+
+  if (dish != null) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 col-md-5 mt-1'>
+            <Card>
+              <CardImg top src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle className='font-weight-bold'>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='col col-md mt-1'>
+            <h4>Comments</h4>
+            <div className='font-weight-bold'>
+              <RenderComment dishSelected={dish} />
             </div>
           </div>
         </div>
-      );
-    } else {
-      return <div></div>;
-    }
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
-}
+};
 
 export default DishDetail;
