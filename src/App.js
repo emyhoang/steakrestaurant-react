e65896f30904678a4cff3caadbf6b38591a009e6
Louis <emyhoang8899@gmail.com>
2020-04-29 15:59:38 -0700

    setup redux store and reducers
    - yarn install redux and react-redux
    - create new folder redux in src folder
    - create new file configureStore.js and use createStore() method
    - add reducer.js file that sets initial state and returns a reducer function (for now just returning the store, no action yet)
    - import provider in app.js, create variable 'store' and initialize the ConfigureStore(), and use provider with parameter store when rendering react component
    - connect the redux store to use the state in main component by importing withRouter from react-router-dom and connect from react-redux
    - mapStateToProps

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
         <>
           <Main />
         </>
       </BrowserRouter>
+
+      </Provider>
     );
   }
 }
