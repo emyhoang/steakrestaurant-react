f6fc4968534c818958d69b4c22b4079eeb06912c
Louis <emyhoang8899@gmail.com>
2020-05-14 16:52:40 -0700

    update the Redux actions and the Home and About components to use the data from the server for rendering the leader information

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,7 +1,27 @@
-import { LEADERS } from '../shared/leaders';
+// import { LEADERS } from '../shared/leaders';
+import * as ActionType from './ActionTypes';
 
-export const Leaders = (state = LEADERS, action) => {
+export const Leaders = (state = { isLoading: true, errMess: null, leaders: [] }, action) => {
   switch (action.type) {
+    case ActionType.ADD_LEADERS:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        leaders: action.payload,
+      };
+    case ActionType.LEADERS_FAILED:
+      return {
+        ...state,
+        errMess: action.payload,
+      };
+    case ActionType.LEADERS_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        errMess: null,
+        leaders: [],
+      };
     default:
       return state;
   }
